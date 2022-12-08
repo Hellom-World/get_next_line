8975921db39331a851e0dccadab5e725bee8ae8a
Hellom Oliveira Mendes <heolivei@c1r8s12.42porto.com>
2022-12-08 17:03:01 +0000

    configuracao inicial

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 15:37:47 by heolivei          #+#    #+#             */
+/*   Updated: 2022/12/08 15:51:01 by heolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	LIBFT_H //para evitar que o mesmo arquivo de cabecalho seja adicionado varias vezes
+# define LIBFT_H
+
+//Declarar as funcoes
+
+#endif
