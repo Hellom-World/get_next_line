7f4145a4ee32a3f43e22263314e1e58832d39035
Hellom Oliveira Mendes <heolivei@c1r8s12.42porto.com>
2022-12-08 18:56:07 +0000

    configura main

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:37:47 by heolivei          #+#    #+#             */
-/*   Updated: 2022/12/08 15:51:01 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/12/08 17:34:37 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	LIBFT_H //para evitar que o mesmo arquivo de cabecalho seja adicionado varias vezes
 # define LIBFT_H
 
-//Declarar as funcoes
+#include <stdio.h>
 
 #endif
--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/08 15:37:23 by heolivei          #+#    #+#             */
-/*   Updated: 2022/12/08 17:39:11 by heolivei         ###   ########.fr       */
+/*   Created: 2022/12/08 17:39:48 by heolivei          #+#    #+#             */
+/*   Updated: 2022/12/08 18:55:38 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <unistd.h>
 
-char	*get_next_line(int fd)
+int	main(void)
 {
-	
+	FILE	*file;
+	int	fd;
+
+	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Nao foi possivel ler o arquivo");
+		exit (0);
+	}
+	printf("\n %i", fd);
+
+	close (fd);
+	return (0);
 }
