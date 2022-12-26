1fb00295940d615010875ed3aee3a56682c6377c
Hellom Oliveira Mendes <heolivei@c1r8s14.42porto.com>
2022-12-26 18:58:52 +0000

    desenvolvento o get_next_line a serio

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,34 @@
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:37:23 by heolivei          #+#    #+#             */
-/*   Updated: 2022/12/17 20:39:50 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/12/26 18:58:01 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	char	*read_frase;
-	static char	*frase;
-	long int	int_read;
-	int		j;
-	int		k;
-	
+	char		*liquid_line;
+	static char	*brute_line;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	read_frase = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if(!read_frase)
+	brute_line = ft_brute_line(fd, brute_line)
+	if (!brute_line)
 		return (0);
-	int_read = 1;
-	if(!frase)
-		frase = malloc(sizeof(char));
-	j = 0;
-	while (int_read != 0)
-	{
-		int_read = read(fd, read_frase, BUFFER_SIZE);
-		read_frase[int_read] = '\0';
-		frase = ft_strjoin(frase, read_frase);
-		if (ft_strchr(read_frase, '\n'))
-			break;
-	}
-	while (frase[j] != '\n')
-		j++;
-	
-		while (frase[j] != '\0')
-		{
-			frase[j] = 0;
-			j++;
-		}
-	return (frase);
+	liquid_line = ft_liquid_line(brute_line)
+	brute_line = rest_line(brute_line);
+	return (liquid_line);
+}
+char	*brute_line(int fd, char *brute_line)
+{
+	char	*vl;
+	int	nbl;
+
+	vl = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!vl)
+		return (0);
+	nbl = 1;
+	while ()
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:39:48 by heolivei          #+#    #+#             */
-/*   Updated: 2022/12/17 20:36:15 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/12/21 13:26:46 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,14 @@ int	main(void)
 	int	i;
 	
 	fd = open("test.txt", O_RDONLY);
-	i = 1;
+	
 	while (1)
-	{
+	{	
 		line = get_next_line(fd);
-		printf("%s", line);
 		if (!line)
-			break ;
+			break;
+		printf("%s", line);
 		free(line);
-		i++;
 	}
 	close (fd);
 	return (0);
